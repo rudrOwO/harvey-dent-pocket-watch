2b530a0e09917b77332613093409c08b994d10a0
rudrOwO <rudro1152@gmail.com>
2022-01-08 21:55:17 +0600

    improved styling + solved canvas dimension caveat

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 // Background #21082c
 // Clock Foreground #f2dbf0
+
+const proportion = 0.9;
+const dimension = Math.min(575, window.innerHeight, window.innerWidth);
+
+const canvas = document.getElementById("harvey-canvas");
+canvas.width = `${dimension * proportion}`;
+canvas.height = `${dimension * proportion}`;
+
+const ctx = canvas.getContext("2d");
+const harveyImage = new Image(canvas.width, canvas.height);
+harveyImage.src = "../assets/harvey.png";
+
+harveyImage.addEventListener("load", () => {
+	ctx.drawImage(harveyImage, 0, 0);
+});
+
+console.log(dimension * proportion);
