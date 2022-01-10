9d4fd34ee90f7380f1a7f217b412a4cb65da5ed2
rudrOwO <rudro1152@gmail.com>
2022-01-10 23:57:36 +0600

    Made use of promises for proper initialization

--- src/app.js
+++ src/app.js
@@ -10,35 +10,56 @@ let mirrorImage = new Image(canvas.width, canvas.height);
 harveyImage.src = "../assets/harvey.png";
 mirrorImage.src = "../assets/mirror.png";
 
+// This array of promises ensures that initialization code is run only after both images have finished loading
+const imagesLoaded = [
+    new Promise((resolve) => {
+        harveyImage.addEventListener("load", () => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        mirrorImage.addEventListener("load", () => {
+            resolve();
+        });
+    }),
+];
+
 // Setting up canvas properties
-const proportion = 0.9; // This must remain consistent with CSS rules
+const proportion = 0.9; // This must remain consistent with CSS Flexbox proportions
 const dimension =
-	Math.min(575, window.innerHeight, window.innerWidth) * proportion;
+    Math.min(575, window.innerHeight, window.innerWidth) * proportion;
 canvas.width = `${dimension}`;
 canvas.height = `${dimension}`;
 const radius = dimension / 2.15;
 const center = {
-	x: dimension / 2,
-	y: dimension / 2,
+    x: dimension / 2,
+    y: dimension / 2,
 };
 const ctx = canvas.getContext("2d");
 ctx.strokeStyle = foreground;
 ctx.fillStyle = foreground;
-ctx.lineWidth = 2;
 ctx.lineCap = "round";
 
 // Setting up clock properties
 const secondHandLength = 0.85 * radius;
 const minuteHandLength = secondHandLength / 1.618;
 const hourHandLength = minuteHandLength / 1.618;
-const boopLength = radius / 50;
+const boopLength = radius / 45;
 
-harveyImage.addEventListener("load", () => {
-	// Inialization code to be run on image load
-	ctx.drawImage(harveyImage, 0, 0, dimension, dimension);
-	ctx.drawCircle(center.x, center.y, radius);
-	ctx.drawCircle(center.x, center.y, boopLength);
-	ctx.fill();
+// Inialization callback to be run once all images have finished loading
+Promise.all(imagesLoaded).then(() => {
+    ctx.drawImage(harveyImage, 0, 0, dimension, dimension);
+    ctx.lineWidth = 2;
+    // Caveat : These two lines work only because they are executed asynchronously
+    ctx.drawCircle(radius);
+    ctx.drawCircle(boopLength);
+    ctx.fill();
+    ctx.drawHand(1.6 * Math.PI, secondHandLength);
+    ctx.lineWidth = 3;
+    ctx.drawHand(0, minuteHandLength);
+    ctx.lineWidth = 4;
+    ctx.drawHand(0.5 * Math.PI, hourHandLength);
+    ctx.drawHand(Math.PI, hourHandLength);
 });
 
-const renderClockTick = function () {};
+function renderClockTick() {}
--- src/utility.js
+++ src/utility.js
@@ -1,7 +1,14 @@
-ctx.drawCircle = function (x, y, radius) {
-	ctx.moveTo(x, y);
-	ctx.beginPath();
-	ctx.arc(x, y, radius, 0, 2 * Math.PI);
-	ctx.stroke();
-	ctx.closePath();
+// Adding utility properties to canvas drawing context
+ctx.drawCircle = function (radius) {
+    ctx.beginPath();
+    ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+};
+
+// uses length and angle - which were determined previously
+ctx.drawHand = function (angle, length) {
+    ctx.beginPath();
+    ctx.arc(center.x, center.y, length, angle, angle);
+    ctx.lineTo(center.x, center.y);
+    ctx.stroke();
 };
